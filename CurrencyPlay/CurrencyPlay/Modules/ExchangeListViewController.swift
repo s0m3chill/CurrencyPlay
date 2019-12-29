627e8f9ceace6de85155ea1d47456648eb709eae
Dariy Kordiyak <dariy.kordiyak@eleks.com>
2019-12-29 14:14:23 +0200

    Re-structure project

---
+++ CurrencyPlay/CurrencyPlay/Modules/ExchangeListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ExchangeListViewController.swift
+//  CurrencyPlay
+//
+//  Created by Dariy Kordiyak on 29.12.2019.
+//  Copyright © 2019 Dariy Kordiyak. All rights reserved.
+//
+
+import UIKit
+
+class ExchangeListViewController: UIViewController {
+    
+    override func loadView() {
+        super.loadView()
+        
+    }
+
+}
+
